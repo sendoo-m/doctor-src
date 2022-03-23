52206193618dd31488071f425e29e8e9b1c9df20
Sendoo <95181522+sendoo-m@users.noreply.github.com>
2022-03-23 21:03:03 +0200

    Updated to heroku

--- doctor/settings.py
+++ doctor/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_summernote',
+    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'patient',
     'crispy_forms',
-    'django_summernote'
+    
 ]
 
 MIDDLEWARE = [
@@ -76,14 +80,19 @@ WSGI_APPLICATION = 'doctor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'base_site.sqlite3'),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -132,6 +141,7 @@ MEDIA_ROOT = os.path.join( BASE_DIR / 'media')
 # MEDIA_URL = '/media/'  it work
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') it work
 
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- patient/admin.py
+++ patient/admin.py
@@ -1,18 +1,33 @@
 from django.contrib import admin
 from django.utils.html import mark_safe
 from django_summernote.admin import SummernoteModelAdmin    # to import edit text box and tool
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportActionModelAdmin
+from import_export.fields import Field
+
 
 from .models import Add_patient
 
 # Register your models here.
 
-class AddAdmin(SummernoteModelAdmin):
+class PatientAdmin(ImportExportActionModelAdmin):
+    pass
+
+class AddAdmin(SummernoteModelAdmin, ImportExportModelAdmin):
 
     search_fields = ['name', 'file_number']
     list_display = ['name', 'file_number', 'idnational']
     list_filter = ['name']
     summernote_fields = ['diagnosis','treatment','note'] # to edit text box and tool
 
+class PatientResource(resources.ModelResource):
+    idnational = Field()
 
-admin.site.register(Add_patient,AddAdmin)
+    class Meta:
+        model = Add_patient
+
+admin.site.register(Add_patient, AddAdmin)
+# admin.site.register(Add_patient, PatientAdmin)
+# admin.site.register(Add_patient, PatientResource)
 
