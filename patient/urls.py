d8ef193420ad7e9080a0a4bb67663d6f5bceeca1
Sendoo <95181522+sendoo-m@users.noreply.github.com>
2022-03-24 21:26:57 +0200

    add staticefiles

--- patient/urls.py
+++ patient/urls.py
@@ -2,12 +2,14 @@ from django.urls import path, include
 from .views import result, add_patient, home, edit_patient
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 app_name = 'patient'
 urlpatterns = [
     path('', home, name='home'),
     path('add_patient', add_patient, name='add_patient'),
     path('patient/result', result, name='result'),
-    # path('patient/edit', edit_patient, name='edit'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('image/favicon.ico'))),
     path('<int:id>/edit', edit_patient, name='edit'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
