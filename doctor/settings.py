f5dfc50b788f2a8dfd5d3450f6d8317b99ac5579
Sendoo <95181522+sendoo-m@users.noreply.github.com>
2022-03-27 23:15:03 +0200

    up postgres

--- doctor/settings.py
+++ doctor/settings.py
@@ -81,17 +81,22 @@ WSGI_APPLICATION = 'doctor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'base_site.sqlite3'),
 #     }
 # }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'base_site.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elgafydb',
+        'USER': 'postgres',
+        'PASSWORD': '159850!@#se',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 # Password validation
